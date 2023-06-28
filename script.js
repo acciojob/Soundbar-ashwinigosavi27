224a30dc203cb83270650b9204fdc29afcdd6ad9
Ashwini Gosavi <120784052+ashwinigosavi27@users.noreply.github.com>
2023-06-28 21:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
 //your JS code here. If required.
-let audioArr=["applause","boo","gasp","tada","victory","wrong"];
-
-let activeAudio=null;
-let btns=document.getElementsByTagName('button');
-for( let i=0;i<btns.length;i++){
-    btns[i].addEventListener('click',soundFunc)
+var audio = new Audio();
+function playSound(filename) {
+	setTimeout(()=>{
+	audio.src = 'sounds/' + filename;
+    audio.play();
+	},4000);
+	
 }
-
-function soundFunc(event){
-const audioName=event.target.innerText;
-// console.log(audioName);
-
-if(audioArr.includes(audioName)){
-if(activeAudio){
-    pause();
-}
-let audio=document.createElement('audio');
-audio.src=`sounds/${audioName}.mp3`;
-audio.controls=true;
-activeAudio=audio;
-play();
-}
-else{
-    pause();
-}
-
-}
-
-
-function play(){
-    activeAudio.play();
-}
-function pause(){
-    if(activeAudio){
-    activeAudio.pause();
-    activeAudio.currentTime=0;
-    }
+function stopSound() {
+	audio.pause();
+    audio.currentTime = 0;
 }
